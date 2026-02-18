f0a6bd9a0d8dc625a305038ca6880cbe600cf8e4
felixOliveros <oliverosfelix@gmail.com>
2026-02-18 02:47:37 +0100

    errores corregidos

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,8 +2,8 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from src.api.models import db, User, People, Planet, Vehicle, Spaceship, Species, Film, Favorite, Todo
-from src.api.utils import generate_sitemap, APIException
+from api.models import db, User, People, Planet, Vehicle, Spaceship, Species, Film, Favorite, Todo
+from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
